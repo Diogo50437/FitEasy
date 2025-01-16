fc50b46dfae3468a67fd62626ed72877a6fee895
Diogo Lobo <diogo.lobo@engineeringhub.man>
2025-01-16 00:17:54 +0000

    first commit, changed header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,17 +1,25 @@
-import React, { Fragment, type MouseEventHandler, useState } from 'react'
-import { useModalStore } from 'store/useModalStore'
-import { useAuth } from 'contexts/AuthContext'
-import OnlineIndicator from 'components/OnlineIndicator'
-import { AppBar, IconButton, Avatar, Popover, List, ListSubheader, ListItemButton } from '@mui/material'
+import React from 'react'
+import { 
+  AppBar, 
+  IconButton, 
+  Avatar,
+  Box,
+  Toolbar,
+  Typography,
+  Menu,
+  Container,
+  Button,
+  Tooltip,
+  MenuItem
+} from '@mui/material'
+import AdbIcon from '@mui/icons-material/Adb';
+import MenuIcon from '@mui/icons-material/Menu';
 
 interface Props {}
 
 const Header: React.FC<Props> = () => {
-  const { isLoggedIn, account, logout } = useAuth()
+  /*const { isLoggedIn, account, logout } = useAuth()
   const { setCurrentModal } = useModalStore()
-
-  const [anchorEl, setAnchorEl] = useState<(EventTarget & HTMLButtonElement) | null>(null)
-  const [popover, setPopover] = useState(false)
 
   const openPopover: MouseEventHandler<HTMLButtonElement> = (e) => {
     setPopover(true)
@@ -31,39 +39,195 @@ const Header: React.FC<Props> = () => {
   const clickRegister = () => {
     setCurrentModal('REGISTER')
     closePopover()
-  }
+  }*/
+
+  const pages = ['Products', 'Pricing', 'Blog'];
+  const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
 
   return (
-    <AppBar className='header' position='static'>
-      <h1>Web App</h1>
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          
+          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            FitEasy
+          </Typography>
 
-      <IconButton onClick={openPopover}>
-        <OnlineIndicator online={isLoggedIn}>
-          <Avatar src={account?.username || ''} alt={account?.username || 'Guest'} />
-        </OnlineIndicator>
-      </IconButton>
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
 
-      <Popover
-        anchorEl={anchorEl}
-        open={popover}
-        onClose={closePopover}
-        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-      >
-        <List style={{ minWidth: '100px' }}>
-          <ListSubheader style={{ textAlign: 'center' }}>Hello, {account?.username || 'Guest'}</ListSubheader>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
 
-          {isLoggedIn ? (
-            <ListItemButton onClick={logout}>Logout</ListItemButton>
-          ) : (
-            <Fragment>
-              <ListItemButton onClick={clickLogin}>Login</ListItemButton>
-              <ListItemButton onClick={clickRegister}>Register</ListItemButton>
-            </Fragment>
-          )}
-        </List>
-      </Popover>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{ display: { xs: 'block', md: 'none' } }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography sx={{ textAlign: 'center' }}>{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+
+          </Box>
+          
+          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            LOGO
+          </Typography>
+          
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography sx={{ textAlign: 'center' }}>{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+
+          </Box>
+
+        </Toolbar>
+      </Container>
     </AppBar>
+
+
+
+    // <AppBar className='header' position='static'>
+    //   <h1>Fit Easy</h1>
+
+    //   <IconButton onClick={openPopover}>
+    //     <OnlineIndicator online={isLoggedIn}>
+    //       <Avatar src={account?.username || ''} alt={account?.username || 'Guest'} />
+    //     </OnlineIndicator>
+    //   </IconButton>
+
+    //   <Popover
+    //     anchorEl={anchorEl}
+    //     open={popover}
+    //     onClose={closePopover}
+    //     anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+    //     transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+    //   >
+    //     <List style={{ minWidth: '100px' }}>
+    //       <ListSubheader style={{ textAlign: 'center' }}>Hello, {account?.username || 'Guest'}</ListSubheader>
+
+    //       {isLoggedIn ? (
+    //         <ListItemButton onClick={logout}>Logout</ListItemButton>
+    //       ) : (
+    //         <Fragment>
+    //           <ListItemButton onClick={clickLogin}>Login</ListItemButton>
+    //           <ListItemButton onClick={clickRegister}>Register</ListItemButton>
+    //         </Fragment>
+    //       )}
+    //     </List>
+    //   </Popover>
+    // </AppBar>
   )
 }
 
